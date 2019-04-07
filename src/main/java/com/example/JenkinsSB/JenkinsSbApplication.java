c3c44fa5446d6641f92e5126ac155f797fc93afa
HemkantG <gangurdehemkant@gmail.com>
2019-04-07 20:54:47 +0530

    initial commit

---
+++ src/main/java/com/example/JenkinsSB/JenkinsSbApplication.java
@@ -0,0 +1,13 @@
+package com.example.JenkinsSB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsSbApplication.class, args);
+	}
+
+}
